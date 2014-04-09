ce63e8fd12df8bb8c1e5ac7136d0c28b2a8d38ad
Geremy Condra <gcondra@google.com>
2014-04-08 21:14:56 -0700

    Don't blow up on read frontier violations.
    This is a useful debugging message and we'd like to know when it
    happens, but it's more accurately backstopped by the target window
    checksum. We should rely on that instead.
    
    Change-Id: I7658a874a8ab1cf1e047984d5829657041336968

--- syspatch.c
+++ syspatch.c
@@ -123,10 +123,8 @@ static SourceRead *get_source_window_from_file(xd3_source *source) {
     MapState* state = (MapState*) source->ioh;
     source_read->blkno = source->getblkno;
     source_read->length = read_with_map(source_read->data, sizeof(source_read->data), state);
-    if (ftell(state->f) < READ_FRONTIER) {
+    if (ftell(state->f) < READ_FRONTIER)
         printf("read past frontier: %ld < %zu\n", ftell(state->f), READ_FRONTIER);
-        return NULL;
-    }
 
     return source_read;
 }
